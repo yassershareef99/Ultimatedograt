7ed099da675e9dfc09c89ca4712025da4eabc524
yassershareef99 <jjgjjhm@gmail.com>
2025-08-05 23:45:18 +0300

    server.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = 7216830801:AAHSCIXyqEtFAmu_9OhnoQPaPHz1RlGQKFo
+const id = 901802280
 const address = 'https://www.google.com'
 
 const app = express();
